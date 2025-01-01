4f4e3d74f8a05206135f7f80f2c5c9ad852f305d
jahid114 <jahidctgcse@gmail.com>
2025-01-01 07:41:54 +0600

    fix: formatted MergeStringAlternately

--- Programs/mergeStringAlternately.go
+++ Programs/mergeStringAlternately.go
@@ -1,23 +1,22 @@
 // This is the solution of leetcode problem 1768
 
 package main
-import "fmt"
 
-func mergeAlternately( word1 string, word2 string ) string {
-    merged := ""
-     i := 0
+func mergeAlternately(word1 string, word2 string) string {
+	merged := ""
+	i := 0
 
-    for ; i < len(word1) && i < len(word2); i++ {
-        merged += string(word1[i]) + string(word2[i])
-    }
+	for ; i < len(word1) && i < len(word2); i++ {
+		merged += string(word1[i]) + string(word2[i])
+	}
 
-    for ; i < len(word1) ; i++{
-        merged += string(word1[i])
-    }
+	for ; i < len(word1); i++ {
+		merged += string(word1[i])
+	}
 
-    for ; i < len(word2) ; i++{
-        merged += string(word2[i])
-    }
+	for ; i < len(word2); i++ {
+		merged += string(word2[i])
+	}
 
-    return merged 
+	return merged
 }
